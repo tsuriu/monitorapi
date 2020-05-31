f57adb2e55fd322abf3eb2918b4f560cd01f69d6
Tulio Amancio <tulioromeror@gmail.com>
2020-05-31 02:43:05 -0300

    Routes in different files

---
+++ src/routes/mikrotik/pppoe.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+
+var RosApi = require('node-routeros').RouterOSAPI;
+
+
+router.get('/', function(req, res, next) {
+    var services = []
+
+  console.log(req.query)
+
+  var ip = req.query.ip
+  var ifn = req.query.ifn
+
+
+  const conn = new RosApi({
+    host: ip,
+    user: 'tulio',
+    password: '64eu.7!4eu#',
+    keepalive: true
+  });
+  conn.connect()
+    .then(() => {
+      conn.write('/interface/pppoe-server/print')
+        .then(data => {
+          const cnt = data.filter(item => item.service === ifn).length;
+          res.json(cnt)
+          console.log(ip,ifn,cnt)
+          conn.close()
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    })
+    .catch(err => {
+      console.log(err);
+    })
+});
+
+module.exports = router;

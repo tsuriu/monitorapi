fabdde167cf31f9d8ff1072b73b91ae83d35f769
Tulio Amancio <tulioromeror@gmail.com>
2020-12-27 18:53:58 -0300

    removing sensible data

--- tests/mkt_try.py
+++ tests/mkt_try.py
@@ -3,7 +3,12 @@ sys.path.insert(0,'../src/modules')
 
 from mikrotik.mikrotik import mkt
 
-host = mkt('10.82.78.204','tulio','64eu.7!4eu#')
-pppsrv = host.onlinePPP('ftth_arvores')
+host = ""
+user = ""
+passwd = ""
+interface = ""
+
+host = mkt(host, user, passwd)
+pppsrv = host.onlinePPP(interface)
 
 print(pppsrv)
